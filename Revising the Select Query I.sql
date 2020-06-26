dfad05d18ddd236b3a000963c6087e73296436fd
Luciano Andrade <lucianocmandrade@gmail.com>
2020-06-25 18:15:36 -0700

    Add files via upload

---
+++ Revising the Select Query I.sql
@@ -0,0 +1,14 @@
+/*Hackerrank Revising the Select Query I challenge*/
+/*Author: Luciano Carli Moreira de Andrade*/
+
+/*
+Find the challenge:
+https://www.hackerrank.com/challenges/revising-the-select-query/problem
+*/
+
+SELECT 
+    * 
+FROM city 
+WHERE countryCode = 'USA' 
+AND population > 100000;
+
