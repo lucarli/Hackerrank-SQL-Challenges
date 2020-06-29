df323218579887d99f8f826cc3a0ee5f88a2490a
Luciano Andrade <lucianocmandrade@gmail.com>
2020-06-29 08:56:47 -0700

    Add files via upload

---
+++ Employee Names.sql
@@ -0,0 +1,14 @@
+/*
+Hackerrank Employee Names challenge
+Author: Luciano Carli Moreira de Andrade
+
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+Find the challenge:
+https://www.hackerrank.com/challenges/name-of-employees/problem
+*/
+
+SELECT 
+    name 
+FROM employee 
+ORDER by name;
